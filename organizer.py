2b7df69a5b5adea9b03715d54115bab995f1f927
PoojaK15 <pkarakoti15@gmail.com>
2026-05-12 18:40:32 +0530

    Initial commit

---
+++ organizer.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+
+# Your folder path
+path = input("Enter folder path: ").strip()
+
+# Folders to create
+folders = ["Images", "Documents", "Videos"]
+#if os.path.isfile(file): it gives only file name and file are not in current directory so it will not work
+# Step 1: Create folders if not exist
+for folder in folders:
+    folder_path = os.path.join(path, folder)
+    
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
+        print(f"Created folder: {folder}")
+
+# Step 2: Move files
+for file in os.listdir(path):
+    full_path = os.path.join(path, file)
+
+    if os.path.isfile(full_path):
+        name, ext = os.path.splitext(file)
+        ext = ext.lower()   # 🔥 important for .JPG, .PDF etc.
+
+        if ext in [".jpg", ".png", ".jpeg"]:
+            shutil.move(full_path, os.path.join(path, "Images", file))
+            print(f"Moved {file} → Images")
+
+        elif ext in [".pdf", ".txt", ".docx"]:
+            shutil.move(full_path, os.path.join(path, "Documents", file))
+            print(f"Moved {file} → Documents")
+
+        elif ext in [".mp4", ".mkv", ".avi"]:
+            shutil.move(full_path, os.path.join(path, "Videos", file))
+            print(f"Moved {file} → Videos")
